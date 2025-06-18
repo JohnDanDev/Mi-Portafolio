e0dc7ff6328a6f783a23935c64a3fd52620635ee
Juan Daniel <juand.gor@gmail.com>
2025-06-17 22:14:22 -0600

    Se añadio al repo principal

---
+++ MiPortafolioFront/src/app/components/principal/principal.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-principal',
+  imports: [],
+  templateUrl: './principal.component.html',
+  styleUrl: './principal.component.css'
+})
+export class PrincipalComponent{ 
+}
