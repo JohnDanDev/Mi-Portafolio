04f4dafdf77e99615aa4c7f8bca0740fcb2dfd00
Juan Daniel <juand.gor@gmail.com>
2025-06-24 18:44:44 -0600

    Estilos al menu. Funcionalidad

--- MiPortafolioFront/src/main.ts
+++ MiPortafolioFront/src/main.ts
@@ -1,6 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
-import { ProyectoComponent } from './app/components/proyecto/proyecto.component';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
