5369029d615ed3216f31109db73407dfd18738e3
Juan Daniel <juand.gor@gmail.com>
2025-09-03 00:11:53 -0600

    Se va a realizar la parte de autenticacion

--- MiPortafolioFront/src/app/services/auth.service.ts
+++ MiPortafolioFront/src/app/services/auth.service.ts
@@ -1,15 +1,42 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  getUserRole(): string{
-    return localStorage.getItem('role') || 'user';
+  private apiUrl = 'http://localhost:8000/api/login';
+  private loggedIn = new BehaviorSubject<boolean>(false);
+
+  constructor(private http: HttpClient){
+    this.loggedIn.next(!!localStorage.getItem('token'));
   }
 
-  isAdmin(): boolean {
-    return this.getUserRole() === 'admin';
+  //Metodo para hacer login
+  login(credential:{usuario: string, password: string}): Observable<any>{
+    return this.http.post<{token: string}>(this.apiUrl, credential).pipe(
+      tap(response =>{
+        localStorage.setItem('token', response.token);
+        this.loggedIn.next(true);
+      })
+    );
+  }
+  
+  logout(): void{
+    localStorage.removeItem('token');
+    this.loggedIn.next(false);
+  }
+
+  //Saber si esta logueado
+  isLoggedIn(): Observable<boolean>{
+    return this.loggedIn.asObservable();
+  }
+
+  //Obtener token
+  getToken(): string | null {
+    return localStorage.getItem('token');
   }
 }
